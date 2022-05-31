699bd57e6c5936d24ec93b96f0ae36de4fb42aa8
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2022-05-31 16:24:43 +0200

    add constant file

---
+++ NewsFeed/Modules/Buiness Manager/Constant/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NewsFeed
+//
+//  Created by Eslam Abotaleb on 31/05/2022.
+//
+
+import Foundation
+
+struct ConstantURL {
+    static let baseUrl = "https://saurav.tech/NewsAPI/everything/cnn.json"
+
+}
+
