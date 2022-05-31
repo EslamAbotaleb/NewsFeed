d6a86653f77a9293f85b719513fb79cc0da7c2eb
eslamabotaleb07 <eslamabotaleb07@gmail.com>
2022-05-31 18:31:16 +0200

    prepare business logic api into news feed view model

---
+++ NewsFeed/Modules/View Models/NewsFeedViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  NewsFeedViewModel.swift
+//  NewsFeed
+//
+//  Created by Eslam Abotaleb on 31/05/2022.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+final class NewsFeedViewModel {
+    
+    func displayNewsFeed(completion: @escaping(NewsFeed?) -> ()){
+        
+        let resource = Resource<NewsFeed>(url: URL(string: ConstantURL.baseUrl)!)
+        
+       _  = URLRequest.load(resource: resource)
+            .subscribe { news in
+                if news.element?.articles != nil {
+                    completion(news.element)
+                } else {
+                    completion(nil)
+                }
+            }
+
+    }
+    
+}
